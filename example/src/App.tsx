87ac680a08511dba921bf31c7ca62a120fd4da7d
Sagnick Kundu <sagnickkundu97@gmail.com>
2023-07-19 22:34:40 +0530

    added example

---
+++ example/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {TestComponent} from '../../src';
+function App() {
+  return (
+    <div>
+      <TestComponent heading="Hi" content="I am Sagnick" />
+    </div>
+  );
+}
+
+export default App;
