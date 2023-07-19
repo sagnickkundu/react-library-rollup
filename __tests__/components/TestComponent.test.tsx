90a3fb10b17f0c9a26ce49be791ad6c603853953
Sagnick Kundu <sagnickkundu97@gmail.com>
2023-07-19 21:58:06 +0530

    added lint

--- __tests__/components/TestComponent.test.tsx
+++ __tests__/components/TestComponent.test.tsx
@@ -1,36 +1,36 @@
-import React from "react";
-import {render, within} from "@testing-library/react";
+import React from 'react';
+import {render, within} from '@testing-library/react';
 
-import {TestComponent} from "../../src";
-import {TestComponentProps} from "../../src/components/TestComponent/types";
+import {TestComponent} from '../../src';
+import {TestComponentProps} from '../../src/components/TestComponent/types';
 
-describe("TestComponent", () => {
+describe('TestComponent', () => {
   const renderComponent = ({heading, content}: Partial<TestComponentProps>) =>
     render(
       <TestComponent
-        heading={heading || "Default heading text"}
+        heading={heading || 'Default heading text'}
         content={content || <div>Default content</div>}
       />
     );
 
-  it("should render heading text correctly", () => {
-    const headingText = "Some test heading";
+  it('should render heading text correctly', () => {
+    const headingText = 'Some test heading';
 
     const {getByTestId} = renderComponent({heading: headingText});
 
-    const testComponent = getByTestId("test-component__heading");
+    const testComponent = getByTestId('test-component__heading');
 
     expect(testComponent).toHaveTextContent(headingText);
   });
 
-  it("should render content correctly", () => {
+  it('should render content correctly', () => {
     const {getByTestId} = renderComponent({
       content: <div data-testid="some-test-content">I am test content</div>,
     });
 
     expect(
-      within(getByTestId("test-component__content")).queryByTestId(
-        "some-test-content"
+      within(getByTestId('test-component__content')).queryByTestId(
+        'some-test-content'
       )
     ).toBeInTheDocument();
   });
